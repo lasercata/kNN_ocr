beca4309c0f65acb1f52bc08f46ed891adb3feaf
lasercata <67599917+lasercata@users.noreply.github.com>
2023-03-04 16:13:51 +0100

    Adding part 2

---
+++ src/seq_test.ml
@@ -0,0 +1,39 @@
+(*type 'a node =
+    | Nil
+    | Cons of 'a * 'a t
+and 'a t = unit -> 'a node;;*)
+
+(*Function that print a 'a Seq.t list.*)
+let rec print_seq (s : 'a Seq.t) : unit =
+    match s() with
+    | Nil -> Printf.printf "\b\b  \b\b\n"
+    | Cons(a, b) -> Printf.printf "%d, " a; print_seq b;;
+
+
+(*Q2 : s = 17 -> 42 -> Nil*)
+let s = (fun () -> (Seq.Cons (17, (fun () -> (Seq.Cons (42, (fun () -> Seq.Nil)))))));;
+
+(*s' = 17 -> 42 -> 17 -> ...*)
+let rec s' = (fun _ -> Seq.Cons (17, (fun _ -> Seq.Cons (42, s'))));;
+(*Seq.iter (fun i -> Printf.printf "%d, " i) s';;*)
+
+
+(*Q3*)
+(*Function that recursively builds the sequence i -> (i + 1) -> (i + 2) -> ...*)
+let rec seq_next_node (i : int) : int Seq.node =
+    Seq.Cons (i, fun _ -> (seq_next_node (i + 1)));;
+
+let all_int = fun _ -> seq_next_node 0;;
+
+
+(*Q4*)
+(*Function that return a -> a + 1 -> ... -> b - 1 -> Nil*)
+let python_range (a : int) (b : int) : int Seq.t =
+    let rec rng_node (i : int) : int Seq.node =
+        match i with
+        | i when i >= b -> Nil
+        | _ -> Seq.Cons (i, fun _ -> (rng_node (i + 1)))
+    in fun _ -> rng_node a;;
+
+print_seq (python_range 3 8);;
+
